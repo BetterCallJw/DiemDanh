46466751e42f10c628c50d4074a0fe26b95907ee
BetterCallJw <145421561+BetterCallJw@users.noreply.github.com>
2024-10-10 21:35:52 +0700

    Delete Program.cs

--- Program.cs
+++
@@ -1,8 +0,0 @@
-public void ConfigureServices(IServiceCollection services)
-{
-    // Thêm HttpClient vào dịch vụ
-    services.AddHttpClient();
-
-    // Thêm dịch vụ MVC hoặc Controllers
-    services.AddControllers();
-}

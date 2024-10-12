047e070a9368ba9b84e5df6d5114f801bc26bdd2
BetterCallJw <145421561+BetterCallJw@users.noreply.github.com>
2024-10-12 09:19:02 +0700

    Delete LocationModel.cs

--- LocationModel.cs
+++
@@ -1,9 +0,0 @@
-public class LocationModel
-{
-    public string Name { get; set; }
-    public string MSSV { get; set; }
-    public double Latitude { get; set; }
-    public double Longitude { get; set; }
-    public int LatDifference { get; set; }
-    public int LonDifference { get; set; }
-}
